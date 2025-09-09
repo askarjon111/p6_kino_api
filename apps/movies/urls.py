89edc9495a7fe33b20e65b9d3efc4b90ba7f4145
ASKAR <askarjon.abdullayev@gmail.com>
2025-09-09 19:44:05 +0500

    movie detail view

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from apps.movies.api_endpoints import genre
+from apps.movies.api_endpoints.movie import MovieDetail
 
 urlpatterns = [
+    path('<int:pk>/', MovieDetail.MovieDetailView.as_view(), name='movie_detail'),
     path('genres/', genre.GenreListView.as_view(), name='genres_list'),
 ]
 
